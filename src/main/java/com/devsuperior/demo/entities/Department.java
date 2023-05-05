590d025f15cc7ea2804048fd486a24035fb7fd03
natannascimento <natan.graves@gmail.com>
2023-05-05 20:01:00 -0300

    Primeiro commit java Spring

---
+++ src/main/java/com/devsuperior/demo/entities/Department.java
@@ -0,0 +1,34 @@
+package com.devsuperior.demo.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_department")
+public class Department {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public Department(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    private Department(){
+
+    }
+
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+}
